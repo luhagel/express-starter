f208dcbbfcef833873f485ff0e6bca0a9eeb5613
Luca Hagel <hagel.luca@gmail.com>
2017-01-23 14:50:45 -0800

    Add handlebars

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 var express = require('express')
+var exphbs  = require('express-handlebars');
+
 var app = express()
 
-//MIDDLEWARE
+// ##### MIDDLEWARE #####
+//Static Files
 app.use('/static', express.static('public'))
+//Handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
+// ##### ROUTES #####
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.render('home')
 })
 
+// ##### SERVER #####
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
